96690be861d35ab2e705f032d6790ae414a9e8cf
Jauhen Leskavetz <j.les@arcor.de>
2016-03-19 00:04:30 +0100

    part4: using jshint

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var jshint = require('gulp-jshint');
+
+var packageJSON = require('./package');
+var jshintConfig = packageJSON.jshintConfig;
+jshintConfig.lookup = false;
 
 gulp.task('default', function() {
   console.log('default task started');
@@ -11,4 +16,11 @@ gulp.task('babel', function() {
   return gulp.src('src/**/*.js')
     .pipe(babel({presets: ['es2015']}))
     .pipe(gulp.dest('js5'));
+});
+
+var lintsrc = 'src';
+gulp.task('lint', function() {
+  return gulp.src(lintsrc+'/**/*.js')
+    .pipe(jshint(jshintConfig))
+    .pipe(jshint.reporter('default'));
 });
