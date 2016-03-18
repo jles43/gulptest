2ee9f84a1902e09b2201ddcb624e9ba463007d26
Jauhen Leskavetz <j.les@arcor.de>
2016-03-18 23:50:40 +0100

    part 3: additional commit changed files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 
 gulp.task('default', function() {
   console.log('default task started');
+});
+
+gulp.task('babel', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest('js5'));
 });
